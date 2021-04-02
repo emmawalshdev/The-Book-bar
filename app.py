693232824265961170447d6d05771d78eec095c9
emmahartedev <emmaharte33@outlook.com>
2021-04-02 16:22:51 +0000

    Build POST method for adding genres

--- app.py
+++ app.py
@@ -164,6 +164,19 @@ def get_genres():
     return render_template("genres.html", genres=genres)
 
 
+@app.route("/add_genre", methods=["GET", "POST"])
+def add_genre():
+    if request.method == "POST":
+        genre = {
+            "genre_name": request.form.get("genre_name")
+        }
+        mongo.db.genres.insert_one(genre)
+        flash("New Genre Added")
+        return redirect(url_for("get_genres"))
+
+    return render_template("add_genre.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
