9ca22c5e6f2456360a0765302c8920251929c803
emmahartedev <emmaharte33@outlook.com>
2021-05-16 20:24:34 +0000

    Add 404 page

--- app.py
+++ app.py
@@ -541,6 +541,12 @@ def delete_genre(genre_id):
     return redirect(url_for("get_genres"))
 
 
+# 404 error
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
