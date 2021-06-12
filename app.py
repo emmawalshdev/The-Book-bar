5fbe450fc4a479615b426e167d92b02cc50a73ad
emmahartedev <emmaharte33@outlook.com>
2021-06-12 20:50:53 +0000

    Change debug to False in app.py

--- app.py
+++ app.py
@@ -916,4 +916,4 @@ def access_denied():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
